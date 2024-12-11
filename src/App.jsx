0f2fea889a970d67d815ffd1a64c73177c49aa31
Ovbiebo Oliver <victorlodoliver@gmail.com>
2024-12-11 15:04:00 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Landing from './auth/landing'
+import Signup from './auth/Signup'
+
+function App() {
+
+  return (
+    <>
+{/* <Landing/> */}
+<Signup/>
+    </>
+  )
+}
+
+export default App
